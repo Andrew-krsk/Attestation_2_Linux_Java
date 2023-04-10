b139abefca245a9b2842df79228520eae379a471
Andrew_Ermolenko <Shpinatakos@mail.ru>
2023-04-10 14:41:16 +0700

    Сделал main

--- ToysShop/src/Main.java
+++ ToysShop/src/Main.java
@@ -1,2 +1,30 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Toy> collect = new ArrayList();
+        collect.add(new Toy(1, "Юла", 25));
+        collect.add(new Toy(2, "Мяч", 25));
+        collect.add(new Toy(3, "Мишка", 25));
+        collect.add(new Toy(4, "Пистолет", 25));
+        PrizeMachine machine = new PrizeMachine();
+
+        for (int i = 0; i <= 20; i++) {
+            Random random = new Random();
+            int number = random.ints(0, 4).findFirst().getAsInt();
+            Toy data = collect.get(number);
+            machine.PutData(data);
+        }
+        for (int i = 0; i < 5; i++) {
+            String getPrize = String.valueOf(machine.next());
+            try (FileWriter writer = new FileWriter("prizeToys.txt", true)) {
+                writer.write(getPrize);
+                writer.write('\n');
+                writer.flush();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
